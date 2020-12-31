c7aac109a3fdb375be9299d3edcf9b0e819e1d4a
Igor <n.igor,l@yandex.ru>
2020-12-31 15:40:35 +0400

    19 module done

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// questions block
+const questions = document.querySelectorAll('.questions__item'),
+  questionText = document.querySelectorAll('.questions__text'),
+  questionArrow = document.querySelectorAll('.questions__item img'),
+  answer = document.querySelectorAll('.questions__answers');
+questions.forEach((question, i) => {
+  question.addEventListener('click', () => {
+    answer[i].classList.toggle('questions__answers_hide');
+    if (!answer[i].classList.contains('questions__answers_hide')) {
+      questionText[i].classList.add('questions__answers-active');
+      questionArrow[i].src = 'images/arrow-top.svg';
+    } else {
+      questionText[i].classList.remove('questions__answers-active');
+      questionArrow[i].src = 'images/arrow-down.svg';
+    }
+  });
+});
